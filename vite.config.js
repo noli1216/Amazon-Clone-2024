ae66df79af47cbd870a5670bb7ebcd9f91ae8907
Kurabachew <kmengistu1216@gmail.com>
2025-06-05 20:32:08 -0500

    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Amazon-Clone-2024/",
-  // base: "/",
+  // base: "/Amazon-Clone-2024/",
+  base: "/",
 
   plugins: [react()],
 });
